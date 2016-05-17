5a730a45b134961b4e013011ecf936dc3899ed31
wilcrofter <bill@crofter.net>
2016-05-17 11:12:54 -0400

    export mcmc_mallow

--- src/HitAndRun.jl
+++ src/HitAndRun.jl
@@ -5,6 +5,7 @@ using Mamba
 
 export mcmc_tables, hitrun2chains, chisq_tables, snee, near_random, victorians
 export init_knapsack, mcmc_knapsack
+export mcmc_mallow
 
 include("hrutils.jl")
 include("contingency_tables.jl")
