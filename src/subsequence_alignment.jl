85befb997dd7c6cd9b6495d4ee7abc0b6c92eba0
wilcrofter <bill@crofter.net>
2016-06-21 18:11:40 -0400

    bug fixes--debug statements not removed.

--- src/subsequence_alignment.jl
+++ src/subsequence_alignment.jl
@@ -30,6 +30,11 @@ Single letter codes for the amino acids in proteins. Codes for ambiguous reads a
 """    
 proteins = [Symbol(i) for i in split("G P A V L I M C F Y W H K R Q N E D S T")]
 
+"""
+Default variance
+"""
+default_variance = 20.0
+
 function random_alignment_data(alphabet::Array{Symbol,1}, sequence_length::Int, nr_sequences::Int, deletion_probability::Real; special_symbol=:*)
     A = fill(special_symbol, (nr_sequences, sequence_length))
     # base sequence
@@ -53,7 +58,7 @@ function count_symbols(A::Array{Symbol,2}, alphabet::Array{Symbol,1})
     return M
 end
 
-function log_score_alignment(A::Array{Symbol, 2}, alphabet::Array{Symbol,1}; variance::Float64=1.0)
+function log_score_alignment(A::Array{Symbol, 2}, alphabet::Array{Symbol,1}; variance::Float64=default_variance)
     counts = count_symbols(A, alphabet)
     return -sum(counts.^2)/(2*variance)
 end
@@ -73,35 +78,48 @@ function shift_pos!{T<:Int}(A::Array{Symbol,2}, fixedrow::T, altrow::Array{Symbo
 end
 
 """
+Find the leading positions of all runs of special symbols in the given row.
+"""    
+function find_special_pos(A::Array{Symbol,2}, rw::Int; special_symbol=:*)
+    pos = find(A[rw,:] .== special_symbol)
+    if length(pos) == 0 return pos end
+    for i in length(pos):-1:2
+        if pos[i-1] == pos[i]-1 pos[i] = 0 end
+    end
+    return pos[find(pos .> 0)]
+end
+
+
+"""
 Will alter A in place and return log score.    
 """
-function step_alignment!(current_log_score::Float64, A::Array{Symbol, 2}, alphabet::Array{Symbol,1}; special_symbol::Symbol=:*, variance::Float64=1.0)
+function step_alignment!(current_log_score::Float64, A::Array{Symbol, 2}, alphabet::Array{Symbol,1}; special_symbol::Symbol=:*, variance::Float64=default_variance)
     nrows, ncols = size(A)
     nsym = length(alphabet)
     # Pick a row (other than the first) at random
-    rw = rand(2:nrows)
-    # Pick a special symbol within the row at random. If there are none, return
-    ss = find(special_symbol .== A[rw,:])
-    if length(ss)==0
+    rw = rand(2:nrows); println("rw ",rw, " ", A[rw,:])
+    # Find the lead positions of each block of special symbols within
+    # the row. If there are no such blocks, return
+    ss = find_special_pos(A,rw,special_symbol=special_symbol)
+    if length(ss) == 0
         return current_log_score
     end
+    println("ss ",ss')
     # Generate the log of uniform random variate on [0,1] and add the current
     # log score. The result is equivalent to the log of a uniform random variate
-    # on [0, exp(current_log_score)]. Multiply by twice the variance for
-    # convenience.
-    logu = (log(rand(1))+current_log_score)*2*variance
-    # Choose a special symbol at random in the chosen row.
-    oldpos = rand(ss)
+    # on [0, exp(current_log_score)].
+    logu = log(rand(1))[1]+current_log_score; println("logu ",logu)
+    # Choose a leading special symbol at random in the chosen row.
+    oldpos = rand(ss); println("oldpos ", oldpos)
     # Score all distinct re-positionings of this symbol within the row
     #  For convenience, determine symbol counts exclusive of chosen row
     counts = count_symbols(A[ setdiff(1:nrows, rw), :], alphabet)
     base_score = -sum(counts.^2)
-    #  Score all placements of the chosen symbol. Note that placements
-    #  within a given block of special symbols all result in the same
-    #  sequence. Thus, except for position 1, it suffices to place
-    #  the chosen symbol at positions which follow a symbol in the
-    #  alphabet.
-    positions = A[rw,1] == special_symbol ? vcat(1, find(A[rw,2:ncols] .!= special_symbol)) : find(A[rw,2:ncols] .!= special_symbol)
+    #  Not all alternate positions of the given special symbol will give distinct
+    #  alignments. Distinct alignments correspond to lead positions, ss, and
+    #  to positions occupied by symbols from the alphabet.
+    positions = vcat(ss, find(A[rw,:] .!= special_symbol))
+    println("positions ",positions')
     scores = fill(base_score,length(positions))
     altrow = fill(special_symbol, ncols)
     for i in 1:length(positions)
@@ -110,21 +128,17 @@ function step_alignment!(current_log_score::Float64, A::Array{Symbol, 2}, alphab
         # Compute score. The base score is the summed squares of the counts
         # of distinct symbols in each column, exclusive of those in the row which
         # is currently being altered. The altered row would add 1 to the count,
-        # counts[j, k], of the kth symbol in each column, where k is the index
-        # of the symbol in the altered row. Since
+        # counts[j, k], of the kth symbol in a column, where k is the index
+        # of the symbol in jth column of the altered row. Since
         #        -(counts[j, k]+1)^2 = -counts[j,k]^2 - 2*counts[j,k] - 1
-        # and since counts[j,k]^2 is in the base score, the altered row
+        # and since -counts[j,k]^2 is in the base score, the altered row
         # contributes increments, -1-2*counts[j,k], to the base score. 
         for j in 1:ncols
             k = altrow[j] == special_symbol ? nsym : find(altrow[j] .== alphabet)[1]
-            scores[i] -= 1 + 2*counts[k,j]
+            scores[i] = scores[i]- 1 - 2*counts[k,j]
         end
     end
-    # Choose an altered row at random among those which score as well as logu
-    if any(scores .>= logu) == false
-        println(scores')
-        println(logu)
-    end
+    scores = scores / (2*variance); println("scores ",scores')
     k = rand(find(scores .>= logu))
     newpos = positions[k]
     newsc  = scores[k]
@@ -135,5 +149,5 @@ function step_alignment!(current_log_score::Float64, A::Array{Symbol, 2}, alphab
         A[rw,i]=altrow[i]
     end
     # Return its score
-    return newsc/(2*variance)
+    return newsc
 end
